b4206bc0207111c6dd2701ce3d3ef64dfd00ab6c
BrunoSouzza <souza_0010@hotmail.com>
2021-06-15 01:41:25 -0300

    creating the product component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatSidenavModule } from "@angular/material/sidenav"
 import { MatCardModule } from "@angular/material/card"
 import { MatListModule } from "@angular/material/list"
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component"
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { MatListModule } from "@angular/material/list"
     FooterComponent,
     NavComponent,
     HomeComponent,
+    ProductCrudComponent,
   ],
   imports: [
     BrowserModule,
