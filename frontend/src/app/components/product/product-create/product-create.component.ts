30d4f38b4c1693ca458fc4e70b84bda8583298fb
BrunoSouzza <souza_0010@hotmail.com>
2021-06-22 00:31:50 -0300

    creating table of products

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -10,7 +10,7 @@ import { Product } from "../product.model"
 })
 export class ProductCreateComponent implements OnInit {
   product: Product = {
-    name: "",
+    description: "",
     price: 0,
   }
 
